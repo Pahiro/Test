8f4fabdc166c6d767be93c1a2e4ef4473cb5f081
tertiusvdg <tertiusvdg@gmail.com>
2023-01-02 20:13:12 +0200

    Homework

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', function(req, res) {
   res.render('index', {name: 'Bennet'});
 });
 
+app.get('/tertius', (req, res) => {
+  res.render('tertius', {name: 'All', year: '2023' });
+});
+
+
 app.listen(3000, function() {
   console.log('App listening on port 3000');
 });
