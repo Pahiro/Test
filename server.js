ea9d9a8ab5699171f4d1d9ca634c784d71175e3a
Bennet van der Gryp <bennetvdg@gmail.com>
2023-01-12 21:35:35 +0200

    .env file and styles

--- server.js
+++ server.js
@@ -3,21 +3,25 @@ const ejs = require('ejs');
 const express = require('express');
 
 const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 // Connect to the MariaDB database
 const pool = mariadb.createPool({
   host: 'localhost',
-  user: 'root',
-  password: 'toor',
-  database: 'maria',
+  user: process.env.dbUser,
+  password: process.env.dbPass,
+  database: process.env.dbName,
 });
 
 app.get('/', (req, res) => {
   // Query the "table" table
-  pool.query('SELECT * FROM mytable WHERE line1 = 2').then( rows => {
+  pool.query('SELECT * FROM mytable WHERE line1 = 1').then( rows => {
     res.render('index', { table: rows });
   }).catch( err => {
     console.log(err);
